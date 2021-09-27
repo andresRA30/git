fd2494e029e7d1b50b5dbfe26d10293c55a5facc
Andres Robert <andresxrobert@gmail.com>
2021-09-26 19:50:23 -0600

    POO

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const txtNombre = document.getElementById('txtNombre');
-const txtNombre = document.getElementById('txtApellido')
+const txtApellido = document.getElementById('txtApellido')
 const btnEnviar = document.getElementById('btnEnviar');
 const resultado = document.getElementById('resultado');
 
@@ -8,3 +8,25 @@ btnEnviar.addEventListener("click", () => {
     console.log(txtNombre.value)
     resultado.innerHTML = `<h1>${txtNombre.value}</h1>`
 });
+
+class Persona {
+
+    constructor(nombre, apellido) {
+        this.nombre = nombre
+        this.apellido = apellido
+    }
+}
+class Trabajador extends Persona {
+    constructor(puesto, area) {
+        super('andres', 'robert')
+        this.puesto = puesto
+        this.area = area
+
+    }
+}
+const p = new Persona('andres', 'robert');
+const t = new Trabajador('Desarrollador de software', 'Pagos Digitales Emision');
+
+console.log(p.nombre, p.apellido)
+console.log(t.puesto, t.area, t.nombre, t.apellido)
+console.log(t)
